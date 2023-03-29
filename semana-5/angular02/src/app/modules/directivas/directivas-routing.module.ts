9305a02817dd231851bf77c3d697449b851cd7fe
mishelrodri <rp18027@ues.edu.sv>
2023-03-28 23:29:46 -0600

    🔥 HomeWork - ngFor

--- semana-5/angular02/src/app/modules/directivas/directivas-routing.module.ts
+++ semana-5/angular02/src/app/modules/directivas/directivas-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '@modules/home/pages/home/home.component';
 import { NgIFComponent } from './pages/ng-if/ng-if.component';
 import { NgSwitchComponent } from './pages/ng-switch/ng-switch.component';
+import { NgForComponent } from './pages/ng-for/ng-for.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Home' },
   { path: 'ngif', component: NgIFComponent, title: 'ngif' },
   { path: 'ngswitch', component: NgSwitchComponent, title: 'ngswitch' },
+  { path: 'ngFor', component: NgForComponent, title: 'ngFor' },
 ];
 
 @NgModule({
--- semana-5/angular02/src/app/modules/directivas/pages/ng-for/ng-for.component.ts
+++ semana-5/angular02/src/app/modules/directivas/pages/ng-for/ng-for.component.ts
@@ -3,13 +3,80 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ng-for',
   templateUrl: './ng-for.component.html',
-  styleUrls: ['./ng-for.component.scss']
+  styleUrls: ['./ng-for.component.scss'],
 })
-export class NgForComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NgForComponent {
+  cancionesTaylorSwift = [
+    {
+      titulo: 'Love Story',
+      album: 'Fearless',
+      anio: 2008,
+      duracion: '3:55',
+      genero: 'Country pop',
+    },
+    {
+      titulo: 'You Belong With Me',
+      album: 'Fearless',
+      anio: 2008,
+      duracion: '3:52',
+      genero: 'Pop rock',
+    },
+    {
+      titulo: 'Shake It Off',
+      album: '1989',
+      anio: 2014,
+      duracion: '3:39',
+      genero: 'Pop',
+    },
+    {
+      titulo: 'Blank Space',
+      album: '1989',
+      anio: 2014,
+      duracion: '3:51',
+      genero: 'Electropop',
+    },
+    {
+      titulo: 'Bad Blood',
+      album: '1989',
+      anio: 2014,
+      duracion: '3:19',
+      genero: 'Electropop',
+    },
+    {
+      titulo: 'We Are Never Ever Getting Back Together',
+      album: 'Red',
+      anio: 2012,
+      duracion: '3:11',
+      genero: 'Pop',
+    },
+    {
+      titulo: 'I Knew You Were Trouble',
+      album: 'Red',
+      anio: 2012,
+      duracion: '3:39',
+      genero: 'Electropop',
+    },
+    {
+      titulo: 'Style',
+      album: '1989',
+      anio: 2014,
+      duracion: '3:51',
+      genero: 'Synth-pop',
+    },
+    {
+      titulo: 'Delicate',
+      album: 'Reputation',
+      anio: 2017,
+      duracion: '3:52',
+      genero: 'Electropop',
+    },
+    {
+      titulo: 'Lover',
+      album: 'Lover',
+      anio: 2019,
+      duracion: '3:41',
+      genero: 'Pop',
+    },
+  ];
+  constructor() {}
 }
